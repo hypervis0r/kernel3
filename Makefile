27a32159d07128fbeeb36fd2b5e9fb7f214ed25b
hypervis0r <hypervis0r@phasetw0.com>
2021-11-09 13:38:33 -0800

    ready makefile for kernel

--- Makefile
+++ Makefile
@@ -19,23 +19,33 @@ CFLAGS 	:= -target x86_64-unknown-windows 	\
 		-mno-red-zone 			\
 		-I$(INCLUDE) -I$(EDK2_INCLUDE) -I$(EDK2_INCLUDE_ARCH)
 
-LDFLAGS := -target x86_64-unknown-windows 	\
+BOOT_LDFLAGS := -target x86_64-unknown-windows 	\
 		-nostdlib 			\
 		-Wl,-entry:efi_main 		\
 		-Wl,-subsystem:efi_application 	\
 		-fuse-ld=lld
 
-SRCS = $(shell find $(SRC) -name '*.c')
-OBJS = $(SRCS:.c=.o)
-DEPS = $(shell find $(INCLUDE) -name '*.h')
+BOOT_SRCS := $(shell find $(SRC)/boot -name '*.c')
+BOOT_OBJS := $(BOOT_SRCS:.c=.o)
+BOOT_DEPS := $(shell find $(INCLUDE)/boot -name '*.h')
 
-%.o: %.c $(DEPS)
+KERNEL_SRCS := $(shell find $(SRC)/kernel -name '*.c')
+KERNEL_OBJS := $(KERNEL_SRCS:.c=.o)
+KERNEL_DEPS := $(shell find $(INCLUDE)/kernel -name '*.h')
+
+boot/%.o: %.c $(BOOT_DEPS)
 	$(CC) $(CFLAGS) -c -o $@ $^
 
-BOOTX64.efi: $(OBJS)
-	$(CC) $(LDFLAGS) -o $@ $^
+kernel/%o: %.c $(KERNEL_DEPS)
+	$(CC) $(CFLAGS) -c -o $@ $^
 
-kernel3.img: BOOTX64.efi
+BOOTX64.efi: $(BOOT_OBJS)
+	$(CC) $(BOOT_LDFLAGS) -o $@ $^
+
+kernel3.exe: $(KERNEL_OBJS)
+	$(CC) $(KERNEL_LDFLAGS) -o $@ $^
+
+kernel3.img: BOOTX64.efi # kernel3.exe
 	dd if=/dev/zero of=$@ bs=1k count=1440
 	mformat -i $@ -f 1440 ::
 	mmd -i $@ ::/EFI
@@ -46,7 +56,7 @@ all: kernel3.img
 	@echo "[+] kernel3.img built"
 
 clean:
-	rm -rf $(OBJS) kernel3.img BOOTX64.efi
+	rm -rf $(BOOT_OBJS) $(KERNEL_OBJS) kernel3.img BOOTX64.efi
 
 run: all
 	$(VM) $(VMFLAGS) -hda kernel3.img
--- src/boot/main.c
+++ src/boot/main.c
@@ -5,9 +5,9 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	EFI_STATUS Status;
 	EFI_INPUT_KEY Key;
  
-	/* Store the system table for future use in other functions */
- 
-    /* Say hi */
+    /* 
+		Say hi 
+	*/
 	Status = SystemTable->ConOut->OutputString(SystemTable->ConOut, L"kernel3 bootloader get!\r\n"); // EFI Applications use Unicode and CRLF, a la Windows
 	if (EFI_ERROR(Status))
 	    return Status;
@@ -19,18 +19,22 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     if (EFI_ERROR(Status))
 	    return Status;
  
-    /* Now wait for a keystroke before continuing, otherwise your
-       message will flash off the screen before you see it.
+    /* 
+		Now wait for a keystroke before continuing, otherwise your
+    	message will flash off the screen before you see it.
  
-       First, we need to empty the console input buffer to flush
-       out any keystrokes entered before this point */
-    Status = SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
-    if (EFI_ERROR(Status))
-        return Status;
+    	First, we need to empty the console input buffer to flush
+    	out any keystrokes entered before this point 
+	*/
+	Status = SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
+	if (EFI_ERROR(Status))
+		return Status;
  
-    /* Now wait until a key becomes available.  This is a simple
-       polling implementation.  You could try and use the WaitForKey
-       event instead if you like */
+    /* 
+		Now wait until a key becomes available.  This is a simple
+       	polling implementation.  You could try and use the WaitForKey
+       	event instead if you like 
+	*/
     while ((Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY) ;
  
     return Status;
