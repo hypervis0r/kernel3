b8023d0b871e05631fadfd7812ef766b1cfc0e7a
hypervis0r <hypervis0r@phasetw0.com>
2021-11-09 13:06:45 -0800

    add watchdog timer disable

--- src/boot/main.c
+++ src/boot/main.c
@@ -2,15 +2,22 @@
  
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-    EFI_STATUS Status;
-    EFI_INPUT_KEY Key;
+	EFI_STATUS Status;
+	EFI_INPUT_KEY Key;
  
-    /* Store the system table for future use in other functions */
+	/* Store the system table for future use in other functions */
  
     /* Say hi */
-    Status = SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World\r\n"); // EFI Applications use Unicode and CRLF, a la Windows
+	Status = SystemTable->ConOut->OutputString(SystemTable->ConOut, L"kernel3 bootloader get!\r\n"); // EFI Applications use Unicode and CRLF, a la Windows
+	if (EFI_ERROR(Status))
+	    return Status;
+
+	/*
+		Disable Watchdog Timer
+	*/
+    Status = SystemTable->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
     if (EFI_ERROR(Status))
-        return Status;
+	    return Status;
  
     /* Now wait for a keystroke before continuing, otherwise your
        message will flash off the screen before you see it.
